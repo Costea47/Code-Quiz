b80b4d99ec440dec053a96fb7d91a689cd8ace7b
Constantin <horghidanconstantin91@yahoo.com>
2024-01-16 14:38:42 +0000

    added questions and pseudocoding

---
+++ assets/js/logic.js
@@ -0,0 +1,23 @@
+// reate a code quiz that contains the following requirements:
+
+// * A start button that when clicked a timer starts and the first question appears.
+ 
+//   * Questions contain buttons for each answer.
+//   * 
+//   * When answer is clicked, the next question appears
+//   * 
+//   * If the answer clicked was incorrect then subtract time from the clock
+
+// * The quiz should end when all questions are answered or the timer reaches 0.
+
+//   * When the game ends, it should display their score and give the user the ability to save their initials and their score
+
+
+// pseudo coding
+
+// Create a list of questions with choices and answers
+//Write a function that creates question and answer elements
+//Loop through the questions until time runs out
+//Save the score in the web browser's memory
+//Retrieve score from storage and display on the high scores page
+//End the game when timer reach 0
---
+++ assets/js/questions.js
@@ -0,0 +1,58 @@
+var questions = [
+    {
+        questionTitle:"What is the correct syntax for referring to an external script called 'xxx.js'?",
+        answers: [
+            "<script href='xxx.js'>",
+            "<script name='xxx.js'>",
+            "<script src='xxx.js'>",
+            "<script file='xxx.js'>"
+        ],
+        
+        correctAnswer:"<script src='xxx.js'>",
+    
+        },
+    {
+        questionTitle: "Which of the following is a correct way to declare an array in JavaScript?",
+        answers: [
+            "var arr = [1, 2, 3];",
+            "var arr = (1, 2, 3);",
+            "var arr = 1, 2, 3;",
+            "var arr = {1, 2, 3};"
+        ],
+        correctAnswer: "var arr = [1, 2, 3];"
+    
+    },
+    {
+        questionTitle: "How do you write a conditional statement for executing some statements only if 'i' is NOT equal to 5?",
+        answers: [
+            "if (i <> 5)",
+            "if i != 5",
+            "if (i != 5)",
+            "if i <> 5"
+        ],
+        correctAnswer: "if (i != 5)"
+    
+    },
+    {
+        questionTitle: "Which method is used to round a number to the nearest integer in JavaScript?",
+        answers: [
+            "Math.round()",
+            "Math.floor()",
+            "Math.ceil()",
+            "Math.absolute()"
+        ],
+        correctAnswer: "Math.round()"
+    
+    },
+    {
+        questionTitle: "Which of the following loops will execute the code block at least once, regardless of the condition?",
+        answers: [
+            "for loop",
+            "while loop",
+            "do-while loop",
+            "None of the above"
+        ],
+        correctAnswer: "do-while loop"
+    
+        },
+]
